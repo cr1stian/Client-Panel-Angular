fac4794d73ce74ac810ea693972cbf6ac428bf22
Cristian Herrera <cristianhmal@gmail.com>
2017-11-23 00:37:35 -0800

    "Updated image"

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBpUmZUPC3rcigZtlW_dTJYoaFDqMdGya8",
+    authDomain: "client-panel-fa8b4.firebaseapp.com",
+    databaseURL: "https://client-panel-fa8b4.firebaseio.com",
+    projectId: "client-panel-fa8b4",
+    storageBucket: "client-panel-fa8b4.appspot.com",
+    messagingSenderId: "1012338050486"
+  }
 };
